e20867c5cd673247be61e9740f7dda3ddc313124
hibeenovul <hibeenovul19@gmail.com>
2023-04-25 15:34:52 +0100

    Removing forbidden function calls

--- str_util_advs.c
+++ str_util_advs.c
@@ -1,7 +1,7 @@
-#include "gbk.h"
+#include "main.h"
 
 /**
- *_getline - gets a line of string from a file
+ *_getline - function to gets a line of string from a file
  *@lnptr: variable to store string
  *@size: number of things stored
  *@fd: file stream to read from
@@ -117,7 +117,7 @@ char *_strtok(char *str, const char *delimeter, int whichf)
 {
 	static char *save;
 	char *_new = NULL;
-	int i = 0, (*func)(char *, const char *), loc;
+	int k = 0, (*func)(char *, const char *), loc;
 
 	func = (whichf == 0) ? _strcmpd : _strcmps;
 	loc = (whichf) ? _strlen(delimeter) - 1 : 0;
@@ -126,29 +126,29 @@ char *_strtok(char *str, const char *delimeter, int whichf)
 		if (!save || !*save)
 			return (NULL);
 
-		while (func(save + i, delimeter) != 1 && *(save + i) != '\0')
-			i++;
-		if (*(save + i) == '\0')
+		while (func(save + k, delimeter) != 1 && *(save + k) != '\0')
+			k++;
+		if (*(save + k) == '\0')
 		{
 			_new = save, save = NULL;
 			return (_new);
 		}
 		_new = save;
-		*(save + i) = '\0';
-		save = save + i + loc + 1;
+		*(save + k) = '\0';
+		save = save + k + loc + 1;
 
 		return (_new);
 
 	}
-	while (func(str + i, delimeter) != 1 && *(str + i) != '\0')
-		i++;
+	while (func(str + k, delimeter) != 1 && *(str + k) != '\0')
+		k++;
 
-	if (*(str + i) == '\0')
+	if (*(str + k) == '\0')
 	{
 		save = NULL;
 		return (str);
 	}
-	save = str + i + loc + 1;
-	*(str + i) = '\0';
+	save = str + k + loc + 1;
+	*(str + k) = '\0';
 	return (str);
 }
