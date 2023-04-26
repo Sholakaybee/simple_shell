a77faa678996a152e5185066c125158d10d9529d
hibeenovul <hibeenovul19@gmail.com>
2023-04-26 07:59:14 +0100

    beautiful code

---
+++ beau_code.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - main function is void
+ * Return: 0 when successful
+ */
+int main(void)
+{
+	printf("Here we are betty!");
+		return (0);
+}
